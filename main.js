ca602ac1e10998f0e2dc7b5da09969984a4a1ac6
Fernando Ignacio <Fernando.Ignacio@travelport.com>
2020-11-03 19:34:26 -0300

    onload city

--- main.js
+++ main.js
@@ -8,79 +8,124 @@ SUBSCRIBE HERE: https://home.openweathermap.org/users/sign_up*/
 const apiKey = "4d8fb5b93d4af21d66a2948710284366";
 
 form.addEventListener("submit", e => {
-  e.preventDefault();
-  let inputVal = input.value;
+    e.preventDefault();
+    let inputVal = input.value;
 
-  //check if there's already a city
-  const listItems = list.querySelectorAll(".ajax-section .city");
-  const listItemsArray = Array.from(listItems);
+    //check if there's already a city
+    const listItems = list.querySelectorAll(".ajax-section .city");
+    const listItemsArray = Array.from(listItems);
 
-  if (listItemsArray.length > 0) {
-    const filteredArray = listItemsArray.filter(el => {
-      let content = "";
-      //athens,gr
-      if (inputVal.includes(",")) {
-        //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
-        if (inputVal.split(",")[1].length > 2) {
-          inputVal = inputVal.split(",")[0];
-          content = el
-            .querySelector(".city-name span")
-            .textContent.toLowerCase();
-        } else {
-          content = el.querySelector(".city-name").dataset.name.toLowerCase();
+    if (listItemsArray.length > 0) {
+        const filteredArray = listItemsArray.filter(el => {
+            let content = "";
+            //athens,gr
+            if (inputVal.includes(",")) {
+                //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
+                if (inputVal.split(",")[1].length > 2) {
+                    inputVal = inputVal.split(",")[0];
+                    content = el
+                        .querySelector(".city-name span")
+                        .textContent.toLowerCase();
+                } else {
+                    content = el.querySelector(".city-name").dataset.name.toLowerCase();
+                }
+            } else {
+                //athens
+                content = el.querySelector(".city-name span").textContent.toLowerCase();
+            }
+            return content == inputVal.toLowerCase();
+        });
+
+        if (filteredArray.length > 0) {
+            msg.textContent = `Você já pediu essa previsão ${filteredArray[0].querySelector(".city-name span").textContent
+                } ...ou seja mais específico na pesquisa 😉`;
+            form.reset();
+            input.focus();
+            return;
         }
-      } else {
-        //athens
-        content = el.querySelector(".city-name span").textContent.toLowerCase();
-      }
-      return content == inputVal.toLowerCase();
-    });
 
-    if (filteredArray.length > 0) {
-      msg.textContent = `You already know the weather for ${
-        filteredArray[0].querySelector(".city-name span").textContent
-      } ...otherwise be more specific by providing the country code as well 😉`;
-      form.reset();
-      input.focus();
-      return;
+
+
     }
-  }
 
-  //ajax here
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+    //ajax here
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const { main, name, sys, weather } = data;
-      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
-        weather[0]["icon"]
-      }.svg`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const { main, name, sys, weather } = data;
+            const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]
+                }.svg`;
 
-      const li = document.createElement("li");
-      li.classList.add("city");
-      const markup = `
+            const li = document.createElement("li");
+            li.classList.add("city");
+            const markup = `
         <h2 class="city-name" data-name="${name},${sys.country}">
           <span>${name}</span>
           <sup>${sys.country}</sup>
         </h2>
         <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
         <figure>
-          <img class="city-icon" src="${icon}" alt="${
-        weather[0]["description"]
-      }">
+          <img class="city-icon" src="${icon}" alt="${weather[0]["description"]
+                }">
           <figcaption>${weather[0]["description"]}</figcaption>
         </figure>
         <span>min:${Math.round(main.temp_min)}<sup>°C</sup> max:${Math.round(main.temp_max)}<sup>°C</sup> </span>
       `;
-      li.innerHTML = markup;
-      list.appendChild(li);
-    })
-    .catch(() => {
-      msg.textContent = "Please search for a valid city 😩";
-    });
+            li.innerHTML = markup;
+            list.appendChild(li);
 
-  msg.textContent = "";
-  form.reset();
-  input.focus();
+            //save cities
+            localStorage.setItem('city', inputVal);
+
+        })
+        .catch(() => {
+            msg.textContent = "Please search for a valid city 😩";
+        });
+
+    msg.textContent = "";
+    form.reset();
+    input.focus();
+});
+
+$(window).on('load', function () {
+    const lastcity = localStorage.getItem('city');
+    //alert(lastcity);
+
+    if (lastcity != null) {
+        //ajax here
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${lastcity}&appid=${apiKey}&units=metric`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const { main, name, sys, weather } = data;
+                const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]
+                    }.svg`;
+
+                const li = document.createElement("li");
+                li.classList.add("city");
+                const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${weather[0]["description"]
+                    }">
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+        <span>min:${Math.round(main.temp_min)}<sup>°C</sup> max:${Math.round(main.temp_max)}<sup>°C</sup> </span>
+      `;
+                li.innerHTML = markup;
+                list.appendChild(li);
+
+                //save cities
+                localStorage.setItem('city', inputVal);
+
+            })
+    }
+
 });
